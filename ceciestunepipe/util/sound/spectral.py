1ad8a25f08d8a3f11818d35a9faf9cadd923ca76
zekearneodo <earneodo@ucsd.edu>
2021-11-09 14:18:23 -0800

    added notebook to get bout rasters more directly (sglx_pipe-bout_rasters)

--- ceciestunepipe/util/sound/spectral.py
+++ ceciestunepipe/util/sound/spectral.py
@@ -8,14 +8,14 @@ def make_butter_bandpass(s_f: float, lo_cut:float, hi_cut:float, order: int=5):
     nyq = 0.5 * s_f
     low = lo_cut / nyq
     high = hi_cut / nyq
-    b, a = sg.butter(order, [low, high], btype='band')
+    b, a = signal.butter(order, [low, high], btype='band')
     return {'b': b, 'a': a}
 
 def apply_butter_bandpass(x: np.array, pars: dict, axis: int=0):
-    return sg.filtfilt(pars['b'], pars['a'], x, axis=axis)
+    return signal.filtfilt(pars['b'], pars['a'], x, axis=axis)
 
 
-def ms_spectrogram(x, s_f, n_window=512, step_ms=1, f_min=300, f_max=7000, cut_off=0.000055):
+def ms_spectrogram(x, s_f, n_window=512, step_ms=1, f_min=100, f_max=9000, cut_off=0.000055):
 
     # the overlap is the size of the window minus the smples in a msec
     msec_samples = int(s_f * 0.001)
